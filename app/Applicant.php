29cbc18a0ec8828e94f1b88fd34819baf646def7
Morgan <morgan.rowse@gmail.com>
2016-10-19 14:57:04 +0200

    update applicant morph and add status label

--- app/Applicant.php
+++ app/Applicant.php
@@ -8,6 +8,27 @@ class Applicant extends Model
 {
     public function user()
     {
-        return $this->morphOne('User', 'userable');
+        return $this->hasOne('App\User', 'userable_id');
+    }
+
+    public function getStatusLabel(){
+        switch($this->application_status){
+            case 0:
+                return '<label class="label label-success">'.trans('string.created').'</label>';
+                break;
+            case 1:
+                return '<label class="label label-primary">'.trans('string.unfunded').'</label>';
+                break;
+            case 2:
+                return '<label class="label label-warning">'.trans('string.partially_funded').'</label>';
+                break;
+            case 3:
+                return '<label class="label label-success">'.trans('string.fully_funded').'</label>';
+                break;
+            default:
+                return '<label class="label label-danger">'.trans('string.error').'</label>';
+                break;
+
+        }
     }
 }
