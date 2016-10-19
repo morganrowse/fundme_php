3ad8b7f15c9e5ab98de3f2a986fa213483ccd008
Morgan <morgan.rowse@gmail.com>
2016-10-19 14:57:35 +0200

    add global key set env to debug true

--- config/app.php
+++ config/app.php
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -103,7 +103,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => ']w>nn&Jj8Lc.x<Mz3YghTpTK@7Q>R`a/',
 
     'cipher' => 'AES-256-CBC',
 
@@ -228,7 +228,5 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-
     ],
-
 ];
