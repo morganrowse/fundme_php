9d8a963894ccc0d984cf7882fab97a16614f9aff
Morgan <morgan.rowse@gmail.com>
2016-10-19 14:59:40 +0200

    change home link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('home') }}">
                     Fundme
                 </a>
             </div>
